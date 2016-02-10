62aae1c27acaaabe6fcffe805f08397b2f80141b
Josh Richard <eos.josh.richard@gmail.com>
2016-02-10 11:16:54 -0500

    Update run.sh

--- jenkins/slaves/run.sh
+++ jenkins/slaves/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SLAVE_NAME="${SLAVE_NAME_PREFIX}-`hostname`"
+NAME="`hostname`"
 
 su -p -l \
     -s "/bin/bash" \
-    -c "java -jar /var/tmp/swarm-client.jar -labels \"${SLAVE_LABELS}\" -executors \"4\" -disableSslVerification -name \"${SLAVE_NAME}\" -username \"x\" -password \"x\" -fsroot \"/var/tmp\""
+    -c "java -jar /var/tmp/swarm-client.jar -labels \"${LABELS}\" -executors \"4\" -disableSslVerification -name \"${NAME}\" -username \"x\" -password \"x\" -fsroot \"/var/tmp\""
